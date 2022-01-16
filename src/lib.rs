2f1601c681582e1212282ddaaa40be04789f7266
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-01-15 18:36:38 -0800

    Got a small example working with lib.rs and main.rs

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+pub fn do_someting() {
+    println!("do_something");
+}
+
+pub mod front_of_house {
+    pub fn do_something_else() {
+        println!("do_something_else");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use my_test_lib::do_someting;
+use my_test_lib::front_of_house::do_something_else;
+
 fn main() {
     println!("Hello, world!");
+    do_someting_2();
+
+    // Try to use the library functions
+    do_someting();
+    do_something_else();
+}
+
+fn do_someting_2() {
+    println!("do_something_2");
 }
