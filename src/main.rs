a875a0feed963666681a34c44dd04e3622395ffe
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-01-17 12:34:49 -0800

    Got a simple test case with ndarray working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+// use ndarray::prelude::*;
+use ndarray::Array;
+
 fn main() {
-    println!("Hello, world!");
+    // This the width and height of the tile game we're using.
+    let (w, h) = (3, 3);
+    let n_elts = w * h;
+    println!("Board size: {}x{}", w, h);
+
+    let a = Array::<u8, _>::from_iter(0..n_elts);
+    assert_eq!(a.ndim(), 1); // get the number of dimensions of array a
+    assert_eq!(a.len(), n_elts as usize); // get the number of elements in array a
+    assert_eq!(a.shape(), [n_elts as usize]); // get the shape of array a
+    assert_eq!(a.is_empty(), false); // check if the array has zero elements
+
+    println!("{:?}", a);
 }
