7f4bedb967e980929965569fb38c7c5e85c010ca
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-01-22 15:06:44 -0800

    Starting to implement InMemoryIntSet in out_of_core

--- src/out_of_core.rs
+++ src/out_of_core.rs
@@ -1,5 +1,27 @@
+use std::collections::HashSet;
+
 pub fn say_hello() {
     println!("Hello, say_hello.");
 }
 
-pub trait IntSet {}
+/// A set of integers.
+pub trait IntSet {
+    /// Returns true if the set contains this integer.
+    fn contains(&self, item: usize) -> bool;
+
+    /// Inserts a usize into this struct.
+    fn insert(&mut self, item: usize);
+}
+
+/// A set of integers held in memory.
+pub struct InMemoryIntSet(HashSet<usize>);
+
+impl IntSet for InMemoryIntSet {
+    /// Returns true if the set contains this integer.
+    fn contains(&self, item: usize) -> bool {
+        false
+    }
+
+    /// Inserts a usize into this struct.
+    fn insert(&mut self, item: usize) {}
+}
