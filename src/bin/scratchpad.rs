d863e455be231404a155f6c27af9303fdd609324
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-01-22 14:50:00 -0800

    Added a scratchpad executable
    Added src/bin/scratchpad.rs

---
+++ src/bin/scratchpad.rs
@@ -0,0 +1,8 @@
+// use std::collections::HashSet;
+
+use tile_game::out_of_core;
+
+fn main() {
+    out_of_core::say_hello();
+    // panic!("say_hello()");
+}
--- src/out_of_core.rs
+++ src/out_of_core.rs
@@ -1,3 +1,5 @@
 pub fn say_hello() {
     println!("Hello, say_hello.");
 }
+
+pub trait IntSet {}
