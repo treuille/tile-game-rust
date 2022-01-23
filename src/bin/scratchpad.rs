85bcb499f8af39c401c0ea659e9c8fe43d420fa9
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-01-23 01:43:31 -0800

    Implemented and tested InMemoryHashedItemSet.

--- src/bin/scratchpad.rs
+++ src/bin/scratchpad.rs
@@ -2,21 +2,5 @@
 use tile_game::out_of_core::test;
 
 fn main() {
-    // out_of_core::say_hello();
-
     test::scratchpad();
-    println!("Finished out_of_core::test::scratchpad");
-
-    // panic!("say_hello()");
-    // let int_set = InMemoryIntSet::new();
-    // for i in 0..4 {
-    //     let contains_i = int_set.contains(i);
-
-    //     let hash_i = calculate_hash(&i);
-
-    //     println!("{i} -> {contains_i} ({hash_i})");
-    // }
-
-    // let adrien = "Adrien!!! ";
-    // println!("Hello {adrien}");
 }
--- src/out_of_core.rs
+++ src/out_of_core.rs
@@ -1,40 +1,59 @@
 use std::collections::hash_map::DefaultHasher;
 use std::collections::HashSet;
 use std::hash::{Hash, Hasher};
+use std::marker::PhantomData;
 
-pub fn calculate_hash<T: Hash>(t: &T) -> u64 {
+fn calculate_hash<T: Hash>(t: &T) -> u64 {
     let mut s = DefaultHasher::new();
     t.hash(&mut s);
     s.finish()
 }
 
 /// A set of integers.
-pub trait IntSet {
+trait HashedItemSet {
+    type Item: Hash;
+
     /// Returns true if the set contains this integer.
-    fn contains(&self, item: usize) -> bool;
+    fn contains(&self, item: &Self::Item) -> bool;
 
     /// Inserts a usize into this struct.
-    fn insert(&mut self, item: usize);
+    fn insert(&mut self, item: &Self::Item);
 }
 
 /// A set of integers held in memory.
-pub struct InMemoryIntSet(HashSet<usize>);
+#[derive(Debug)]
+struct InMemoryHashedItemSet<T: Hash> {
+    hashes: HashSet<u64>,
+    phantom: PhantomData<T>,
+}
 
-impl InMemoryIntSet {
+impl<T: Hash> InMemoryHashedItemSet<T> {
     /// Contructor
     pub fn new() -> Self {
-        Self(HashSet::<usize>::new())
+        Self {
+            hashes: HashSet::new(),
+            phantom: PhantomData,
+        }
     }
 }
 
-impl IntSet for InMemoryIntSet {
+impl<T: Hash> HashedItemSet for InMemoryHashedItemSet<T>
+where
+    T: Hash,
+{
+    type Item = T;
+
     /// Returns true if the set contains this integer.
-    fn contains(&self, item: usize) -> bool {
-        false
+    fn contains(&self, item: &Self::Item) -> bool {
+        let the_hash = calculate_hash(item);
+        self.hashes.contains(&the_hash)
     }
 
     /// Inserts a usize into this struct.
-    fn insert(&mut self, item: usize) {}
+    fn insert(&mut self, item: &Self::Item) {
+        let the_hash: u64 = calculate_hash(item);
+        self.hashes.insert(the_hash);
+    }
 }
 
 // #[cfg(test)]
@@ -44,10 +63,36 @@ pub mod test {
     // #[test]
     pub fn scratchpad() {
         println!("Testing scratchpad.");
-        for i in 0..40 {
-            let hash_i = calculate_hash(&i);
-            println!("{i} -> {hash_i}");
+        let mut hash_items = InMemoryHashedItemSet::new();
+        hash_items.insert(&'a');
+        hash_items.insert(&'b');
+        hash_items.insert(&'z');
+        println!("hash_items: {hash_items:?}");
+
+        // for i in TEST_STR.chars().step_by(2) {
+        //     hash_items.insert(&i);
+        // }
+        for i in "abcd".chars() {
+            let contains_i = hash_items.contains(&i);
+            println!("{i} in hash_items : {contains_i}");
         }
-        println!("Finished testing scratchpad!");
+        assert!(hash_items.contains(&'a'));
+        assert!(hash_items.contains(&'b'));
+        assert!(!hash_items.contains(&'c'));
+        assert!(!hash_items.contains(&'d'));
+    }
+
+    #[test]
+    pub fn test_in_memory_hashed_item_set() {
+        let mut hash_items = InMemoryHashedItemSet::new();
+
+        hash_items.insert(&'a');
+        hash_items.insert(&'b');
+        hash_items.insert(&'z');
+
+        assert!(hash_items.contains(&'a'), "Should contain a");
+        assert!(hash_items.contains(&'b'), "Should contain b");
+        assert!(!hash_items.contains(&'c'), "Shouldn't contain c");
+        assert!(!hash_items.contains(&'d'), "Shouldn't contain d");
     }
 }
