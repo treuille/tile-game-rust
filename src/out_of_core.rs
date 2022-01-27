089c07bc519590626847fcf2fd468233ed158f29
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-01-27 02:36:06 -0800

    Starting to build this using a memmap in a temp file

--- src/out_of_core.rs
+++ src/out_of_core.rs
@@ -1,13 +1,16 @@
+use memmap::MmapMut;
+use mktemp::Temp;
 use std::collections::hash_map::DefaultHasher;
 use std::collections::HashSet;
 use std::ffi::OsStr;
-use std::fs::OpenOptions;
+use std::fs::{File, OpenOptions};
 use std::hash::{Hash, Hasher};
+use std::io;
 use std::marker::PhantomData;
 use std::path::PathBuf;
 use std::process::Command;
-use std::str::FromStr;
 
+// temp_file is cleaned from the fs here
 /// A set of integers.
 trait HashedItemSet<T: Hash> {
     /// Returns true if the set contains this integer.
@@ -72,66 +75,104 @@ struct OutOfCoreHashedItemSet<T: Hash> {
     // Where we store the hashes of the elements.
     hashes: HashSet<u64>,
 
-    // Maximum number of elements in memory
-    max_elts: usize,
+    // The memory map we use to back this data structure.
+    mmap: TempMemMap<T>,
+    // 0-sized variable that makes this type behave as if it
+    // contained i tems of type T.
+    // phantom: PhantomData<T>,
+}
 
-    // The memory maps we use to back this data structure.
-    maps: Vec<()>,
+impl<T: Hash> OutOfCoreHashedItemSet<T> {
+    /// Contructor
+    fn new(max_elts: usize) -> io::Result<Self> {
+        // run_command(format!("rm -rfv {}/*", Self::CACHE_PATH));
+        // run_command(format!("mkdir -p {}", Self::CACHE_PATH));
+
+        Ok(Self {
+            hashes: HashSet::new(),
+            mmap: TempMemMap::<T>::new(100)?,
+            // phantom: PhantomData,
+        })
+    }
+
+    // // Where we store the data.
+    // const CACHE_PATH: &'static str = "cache/item_set";
+}
+
+impl<T: Hash> HashedItemSet<T> for OutOfCoreHashedItemSet<T> {
+    ///  jReturns true if the set contains this item.
+    fn contains(&self, _item: &T) -> bool {
+        false
+    }
+
+    /// Inserts an item into the set
+    fn insert(&mut self, item: &T) {
+        let mut path_buf = PathBuf::new();
+        {
+            let temp_file = Temp::new_file().unwrap();
+            path_buf.push(&temp_file);
+            println!("{:?} exists: {}", *temp_file, temp_file.exists());
+            println!("{:?} exists: {}", path_buf, path_buf.exists());
+            println!("{:?} exists: {}", path_buf, path_buf.exists());
+        }
+        println!("{:?} exists: {}", path_buf, path_buf.exists());
+        panic!("All done!");
+        // println!("{:?} exists: {}", path_buf, Path::exists(path_buf));
+        // self.hashes.insert(Self::hash(item));
+
+        // // TODO: This is were I need to start implementing the memory map.
+        // if self.hashes.len() >= self.max_elts {
+        //     println!("There are now {} elements!", self.hashes.len());
+        //     // Create the file
+        //     let mut path = PathBuf::from_str(Self::CACHE_PATH).unwrap();
+        //     path.push(format!("{:05}.dat", self.maps.len()));
+        //     let file = OpenOptions::new()
+        //         .read(true)
+        //         .write(true)
+        //         .create(true)
+        //         .open(&path)
+        //         .unwrap();
+        //     println!(
+        //         "Creating a file of length {}",
+        //         self.max_elts * std::mem::size_of::<u64>()
+        //     );
+        //     file.set_len((self.max_elts * std::mem::size_of::<u64>()) as u64)
+        //         .unwrap();
+        //     println!("Creating cache file: {path:?}");
+        //     panic!("adding the memory map");
+        // }
+    }
+}
+
+/// A Memory map backed by a temporary file.
+struct TempMemMap<T> {
+    // The filename of the memory map.
+    filename: Temp,
+
+    // // The file that holds the memory map.
+    // file: File,
+
+    // The memory map itself.
+    mmap: MmapMut,
 
     // 0-sized variable that makes this type behave as if it
     // contained i tems of type T.
     phantom: PhantomData<T>,
 }
 
-impl<T: Hash> OutOfCoreHashedItemSet<T> {
-    /// Contructor
-    fn new(max_elts: usize) -> Self {
-        run_command(format!("rm -rfv {}/*", Self::CACHE_PATH));
-        run_command(format!("mkdir -p {}", Self::CACHE_PATH));
-
-        Self {
-            hashes: HashSet::new(),
-            max_elts,
-            maps: Vec::new(),
+impl<T> TempMemMap<T> {
+    fn new(n_elts: usize) -> io::Result<Self> {
+        let filename = Temp::new_file()?;
+        let file = OpenOptions::new().read(true).write(true).open(&filename)?;
+        file.set_len((n_elts * std::mem::size_of::<u64>()) as u64)?;
+        let mmap = unsafe { MmapMut::map_mut(&file)? };
+        println!("Created a memory map with: {:?}", file);
+        Ok(Self {
+            filename,
+            // file,
+            mmap,
             phantom: PhantomData,
-        }
-    }
-
-    // Where we store the data
-    const CACHE_PATH: &'static str = "cache/item_set";
-}
-
-impl<T: Hash> HashedItemSet<T> for OutOfCoreHashedItemSet<T> {
-    /// Returns true if the set contains this item.
-    fn contains(&self, item: &T) -> bool {
-        false
-    }
-
-    /// Inserts an item into the set
-    fn insert(&mut self, item: &T) {
-        self.hashes.insert(Self::hash(item));
-
-        // TODO: This is were I need to start implementing the memory map.
-        if self.hashes.len() >= self.max_elts {
-            println!("There are now {} elements!", self.hashes.len());
-            // Create the file
-            let mut path = PathBuf::from_str(Self::CACHE_PATH).unwrap();
-            path.push(format!("{:05}.dat", self.maps.len()));
-            let file = OpenOptions::new()
-                .read(true)
-                .write(true)
-                .create(true)
-                .open(&path)
-                .unwrap();
-            println!(
-                "Creating a file of length {}",
-                self.max_elts * std::mem::size_of::<u64>()
-            );
-            file.set_len((self.max_elts * std::mem::size_of::<u64>()) as u64)
-                .unwrap();
-            println!("Creating cache file: {path:?}");
-            panic!("adding the memory map");
-        }
+        })
     }
 }
 
@@ -140,7 +181,7 @@ pub mod test {
     use super::*;
 
     pub fn scratchpad() {
-        let mut hash_items = OutOfCoreHashedItemSet::new(10);
+        let mut hash_items = OutOfCoreHashedItemSet::new(10).unwrap();
         // use std::mem;
         // println!("Size of hashes: {}", mem::size_of_val(&hash_items.hashes));
         // println!("Size of phantom: {}", mem::size_of_val(&hash_items.phantom));
