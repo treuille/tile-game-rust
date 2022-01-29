caf573c9321f6af5b7d002eaa3d87905284c5821
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-01-28 17:34:08 -0800

    Including the big stack library

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,5 @@
-// The library to store out of core data structures.
+// The library to store big hashed sets of items.
 pub mod out_of_core;
+
+// The library to store items on an out-of-core stack.
+pub mod big_stack;
