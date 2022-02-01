8b3cc04dcef06341b20d30b29878a5bb9c7b286c
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-01-31 17:45:13 -0800

    Got a basic (serial) version of the future parallel loop working.

--- src/bin/search_boards.rs
+++ src/bin/search_boards.rs
@@ -5,6 +5,8 @@ use tile_game::big_stack::{BigStack, Stack};
 
 use tile_game::board::Board;
 
+use std::mem;
+
 fn factorial(x: usize) -> usize {
     (2..=x).fold(1, |x, y| x * y)
 }
@@ -18,8 +20,48 @@ fn main() {
     println!("Anticipated solutions: {n_solns}");
 
     let board = Board::new(0..n_elts, &(w, h));
-    let n_solns = find_all_boards_iteratively(board, n_solns);
+    // let n_solns = find_all_boards_iteratively(board, n_solns);
+    let n_solns = find_all_boards_in_parallel(board);
     println!("There are {} solutions.", n_solns);
+}
+
+/// Counts the number of boards which are accessible from the starting position.
+fn find_all_boards_in_parallel(board: Board) -> usize {
+    let cache_size = 1 << 25;
+    let mut unprocessed_boards: BigStack<Board> = BigStack::new(cache_size);
+    unprocessed_boards.push(board.clone());
+
+    let mut all_boards: BigSet<Board> = BigSet::new(1 << 18);
+
+    while unprocessed_boards.len() != 0 {
+        let mut next_unprocessed_boards: BigStack<Board> = BigStack::new(cache_size);
+
+        while let Some(board) = unprocessed_boards.pop() {
+            if !all_boards.contains(&board) {
+                all_boards.insert(&board);
+                // if all_boards.len() % 1000000 == 0 {
+                //     println!(
+                //         "Processed {} boards with {} to go.",
+                //         all_boards.len(),
+                //         unprocessed_boards.len() + next_unprocessed_boards.len()
+                //     );
+                // }
+                for permuted_board in board.slide_iter() {
+                    // unprocessed_boards.push(permuted_board.clone());
+                    next_unprocessed_boards.push(permuted_board);
+                }
+            }
+        }
+        mem::swap(&mut unprocessed_boards, &mut next_unprocessed_boards);
+        println!(
+            "Finished a rung {} boards with ({}, {}) to go.",
+            all_boards.len(),
+            unprocessed_boards.len(),
+            next_unprocessed_boards.len()
+        );
+    }
+
+    all_boards.len()
 }
 
 /// Counts the number of boards which are accessible from the starting position.
@@ -28,6 +70,7 @@ fn main() {
 ///
 /// * `board` - The starting position.
 /// * `n_solns` - The expected number of solutions.
+#[allow(dead_code)]
 fn find_all_boards_iteratively(board: Board, n_solns: usize) -> usize {
     let mut unprocessed_boards: BigStack<Board> = BigStack::new(1 << 25);
     unprocessed_boards.push(board.clone());
