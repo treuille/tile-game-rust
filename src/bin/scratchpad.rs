2cdce2b84686a9c7889e06e20c47c3b0b0d057de
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-01-31 20:10:32 -0800

    Successfully added rayon and tested it in Cargo

--- src/bin/scratchpad.rs
+++ src/bin/scratchpad.rs
@@ -1,6 +1,18 @@
-// use tile_game::big_set::test;
+use rayon::prelude::*;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     println!("Hello, world.");
-    // test::test_partition_set();
+
+    let numbers: Vec<_> = (0..20).collect();
+    numbers
+        .par_iter()
+        .map(|i| {
+            println!("{i}");
+            let one_second = Duration::from_secs(1);
+            thread::sleep(one_second);
+        })
+        .for_each(drop);
+    // .collect::<Vec<_>>();
 }
