cb5ddc08377844f987f48681b902237eee15b5fb
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-02-04 22:56:02 -0800

    Cleaned out a bunch of old parallel code

--- src/big_set.rs
+++ src/big_set.rs
@@ -1,15 +1,12 @@
-use bloomfilter::Bloom;
 use memmap::MmapMut;
 use mktemp::Temp;
-use std::cell::RefCell;
 use std::collections::hash_map::DefaultHasher;
 use std::collections::HashSet;
 use std::fs::OpenOptions;
 use std::hash::{Hash, Hasher};
+use std::io;
 use std::marker::PhantomData;
 use std::ops::{Deref, DerefMut};
-use std::sync::Mutex;
-use std::{cmp, io, iter};
 
 /// A set of items, stored by their hash values.
 pub trait HashedItemSet<T: Hash> {
@@ -69,74 +66,6 @@ impl<T: Hash> HashedItemSet<T> for LittleSet<T> {
     }
 }
 
-/// Stores a large group of elements by hash, even if they can't fit in main memory.
-pub struct BigSet<T: Hash> {
-    /// In memory store the hashes of the elements.
-    hash_cache: HashSet<u64>,
-
-    /// The maximum number of elements in the cache until transferred into the store.
-    cache_size: usize,
-
-    /// Memmap-backed store of hashes of the elements.
-    hash_store: Option<BigU64Array>,
-
-    /// 0-sized variable that makes this type behave as if it
-    /// contained i tems of type T.
-    phantom: PhantomData<T>,
-}
-
-impl<T: Hash> BigSet<T> {
-    /// Contructor
-    pub fn new(cache_size: usize) -> Self {
-        Self {
-            hash_cache: HashSet::with_capacity(cache_size),
-            cache_size,
-            hash_store: None,
-            phantom: PhantomData,
-        }
-    }
-}
-
-impl<T: Hash> HashedItemSet<T> for BigSet<T> {
-    ///  Returns true if the set contains this item.
-    fn contains(&self, item: &T) -> bool {
-        let item_hash = hash(item);
-        if self.hash_cache.contains(&item_hash) {
-            return true;
-        } else if let Some(hash_store) = &self.hash_store {
-            return hash_store.binary_search(&item_hash).is_ok();
-        }
-        false
-    }
-
-    /// Inserts an item into the set
-    fn insert(&mut self, item: &T) {
-        let item_hash = hash(item);
-        self.hash_cache.insert(item_hash);
-        let stack_cache_size = 1 << 10;
-        if self.hash_cache.len() == self.cache_size {
-            let old_store_len = self.hash_store.as_ref().map_or(0, |s| s.len());
-            let mut new_store = BigU64Array::new(old_store_len + self.cache_size).unwrap();
-            if let Some(old_store) = &self.hash_store {
-                new_store[..old_store_len].copy_from_slice(&old_store);
-            }
-            for (i, item_hash) in self.hash_cache.drain().enumerate() {
-                new_store[old_store_len + i] = item_hash;
-            }
-            new_store.sort();
-            self.hash_store = Some(new_store);
-        }
-    }
-
-    /// Returns the number of elements in this set.
-    fn len(&self) -> usize {
-        match &self.hash_store {
-            Some(hash_store) => self.hash_cache.len() + hash_store.len(),
-            None => self.hash_cache.len(),
-        }
-    }
-}
-
 /// A big array of u64s backed by a large memory-mapped temporary file.
 struct BigU64Array {
     /// The memory map itself.
@@ -173,6 +102,7 @@ impl Deref for BigU64Array {
         u64_array
     }
 }
+
 impl DerefMut for BigU64Array {
     fn deref_mut(&mut self) -> &mut Self::Target {
         let (align_left, u64_array, align_right) = unsafe { self.mmap.align_to_mut::<u64>() };
@@ -181,214 +111,6 @@ impl DerefMut for BigU64Array {
         u64_array
     }
 }
-
-/// Like BigSet, but puts all the items behind a bloom filter for efficiency.
-pub struct BloomSet<T: Hash> {
-    /// The bloom filter to avoid some searches.
-    bloom_filter: Bloom<T>,
-
-    /// The mmmap-backed store of hashed items.
-    big_set: BigSet<T>,
-}
-
-impl<T: Hash> BloomSet<T> {
-    /// Constructs a new BloomSet
-    ///
-    /// # Arguments
-    ///
-    /// * `cache_size` - The maximum number of hashed items to store in memory.
-    /// * `items_count` - The expected number total items stored.
-    /// * `fp_p` - The desired number of false positives in the bloom filter.  
-    pub fn new(cache_size: usize, items_count: usize, fp_p: f64) -> Self {
-        Self {
-            bloom_filter: Bloom::new_for_fp_rate(items_count, fp_p),
-            big_set: BigSet::new(cache_size),
-        }
-    }
-}
-
-impl<T: Hash> HashedItemSet<T> for BloomSet<T> {
-    ///  Returns true if the set contains this item.
-    fn contains(&self, item: &T) -> bool {
-        if self.bloom_filter.check(item) {
-            // There could be a false positive so we have to check explicitly.
-            self.big_set.contains(item)
-        } else {
-            false
-        }
-    }
-
-    /// Inserts an item into the set
-    fn insert(&mut self, item: &T) {
-        self.bloom_filter.set(item);
-        self.big_set.insert(item)
-    }
-
-    /// Returns the number of elements in this set.
-    fn len(&self) -> usize {
-        self.big_set.len()
-    }
-}
-
-/// Like BigSet, but puts all the items behind a bloom filter for efficiency.
-pub struct PartitionSet<T: Hash> {
-    /// The number of partions
-    n_partitions: usize,
-
-    /// The bloom filter to avoid some searches.
-    // partitions: Vec<BloomSet<T>>,
-    partitions: Vec<BigSet<T>>,
-}
-
-impl<T: Hash> PartitionSet<T> {
-    /// Constructs a new PartitionSet
-    ///
-    /// # Arguments
-    ///
-    /// * `cache_size` - The maximum number of hashed items to store in memory.
-    /// * `items_count` - The expected number total items stored.
-    /// * `fp_p` - The desired number of false positives in the bloom filter.  
-    /// * `n_partitions` - The number of memory mapped partitions for this set.
-    pub fn new(cache_size: usize, _items_count: usize, _fp_p: f64, n_partitions: usize) -> Self {
-        assert!(n_partitions > 0, "Must have at least one partition.");
-        // let items_per_partition = cmp::max(items_count / n_partitions, 1);
-        let partitions = (0..n_partitions)
-            // .map(|_| BloomSet::new(cache_size, items_per_partition, fp_p))
-            .map(|_| BigSet::new(cache_size))
-            .collect::<Vec<_>>();
-        Self {
-            n_partitions,
-            partitions,
-        }
-    }
-}
-
-impl<T: Hash> HashedItemSet<T> for PartitionSet<T> {
-    ///  Returns true if the set contains this item.
-    fn contains(&self, item: &T) -> bool {
-        let partition = (hash(item) as usize) % self.n_partitions;
-        self.partitions[partition].contains(item)
-    }
-
-    /// Inserts an item into the set
-    fn insert(&mut self, item: &T) {
-        let partition = (hash(item) as usize) % self.n_partitions;
-        self.partitions[partition].insert(item)
-    }
-
-    /// Returns the number of elements in this set.
-    fn len(&self) -> usize {
-        self.partitions.iter().map(|p| p.len()).sum()
-    }
-}
-
-/// This is a set which can be operated through interior mutability
-pub trait InteriorMutableSet<T>
-where
-    T: Hash,
-{
-    /// Inserts an item the set, returning true if the item had already been inserted.
-    fn insert_check(&self, item: &T) -> bool;
-
-    /// Returns the lengths of this set.
-    fn len(&self) -> usize;
-}
-
-impl<T> InteriorMutableSet<T> for RefCell<BigSet<T>>
-where
-    T: Hash,
-{
-    /// Inserts an item, returning true if the item had *previously* been inserted.
-    fn insert_check(&self, item: &T) -> bool {
-        let mut big_set = self.borrow_mut();
-        if big_set.contains(item) {
-            true
-        } else {
-            big_set.insert(item);
-            false
-        }
-    }
-
-    /// Returns the lengths of this set.
-    fn len(&self) -> usize {
-        self.borrow().len()
-    }
-}
-/// InteriorMutableSet which is able to insert elements in parallel
-pub struct ParallelSet<T>
-where
-    T: Hash,
-{
-    n_partitions: usize,
-    sets: Vec<Mutex<BigSet<T>>>,
-}
-
-impl<T> ParallelSet<T>
-where
-    T: Hash,
-{
-    /// Creates a new parallel set parititioning the data into this nuber of sets.
-    pub fn new(cache_size: usize, n_partitions: usize) -> Self {
-        assert!(n_partitions > 0, "Must have at least one partition.");
-        Self {
-            n_partitions,
-            sets: iter::repeat(())
-                .take(n_partitions)
-                .map(|_| Mutex::new(BigSet::new(cache_size / n_partitions)))
-                .collect(),
-        }
-    }
-
-    /// Creates a new parallel set with relatively prime partitions sizes.
-    pub fn with_prime_caches(cache_size: usize, n_partitions: usize) -> Self {
-        assert!(n_partitions >= 1, "Must have at least one partition.");
-        assert!(n_partitions <= 4, "Cannot have more than four partitions");
-        let primes: &[usize] = &[3, 5, 7, 11][..n_partitions];
-        let prime_sum: usize = primes.iter().sum();
-        Self {
-            n_partitions,
-            sets: primes
-                .iter()
-                .map(|p| {
-                    let cache_size: usize = cache_size * p / prime_sum;
-                    println!("Creating a BigSet with cache size {}.", cache_size);
-                    Mutex::new(BigSet::new(cache_size))
-                })
-                .collect(),
-        }
-    }
-}
-
-/// InteriorMutableSet which is able to insert elements in parallel
-impl<T> InteriorMutableSet<T> for ParallelSet<T>
-where
-    T: Hash,
-{
-    /// Inserts an item the set, returning true if the item had already been inserted.
-    fn insert_check(&self, item: &T) -> bool {
-        let partition = (hash(item) as usize) % self.n_partitions;
-        let mut set = self.sets[partition].lock().unwrap();
-        if set.contains(item) {
-            true
-        } else {
-            set.insert(item);
-            false
-        }
-    }
-
-    /// Returns the lengths of this set.
-    fn len(&self) -> usize {
-        // Hold all the locks at once
-        self.sets
-            .iter()
-            .map(|m| m.lock().unwrap())
-            .collect::<Vec<_>>()
-            .iter()
-            .map(|s| s.len())
-            .sum()
-    }
-}
-
 #[cfg(test)]
 pub mod test {
     use super::*;
@@ -410,58 +132,5 @@ pub mod test {
     #[test]
     pub fn test_little_set() {
         test_hashed_item_set(&mut LittleSet::new());
-    }
-
-    #[test]
-    pub fn test_big_set() {
-        test_hashed_item_set(&mut BigSet::new(3));
-    }
-
-    #[test]
-    pub fn test_bloom_set() {
-        test_hashed_item_set(&mut BloomSet::new(3, 26, 0.5));
-    }
-
-    #[test]
-    pub fn test_partition_set() {
-        test_hashed_item_set(&mut PartitionSet::new(3, 26, 0.5, 2));
-    }
-
-    #[test]
-    pub fn test_ref_cell_set() {
-        let set = RefCell::new(BigSet::new(25));
-        test_interior_mutable_set(set);
-    }
-
-    #[test]
-    pub fn test_parallel_set() {
-        let cache_size = 8;
-        let n_partitions = 4;
-        let set = ParallelSet::new(cache_size, n_partitions);
-        test_interior_mutable_set(set);
-    }
-
-    pub fn test_interior_mutable_set(set: impl InteriorMutableSet<usize>) {
-        let max_elts = 1028;
-        for i in (0..max_elts).step_by(4) {
-            assert_eq!(set.len(), i / 4);
-            assert!(!set.insert_check(&i));
-        }
-
-        for i in (0..max_elts).step_by(2) {
-            match i % 4 {
-                0 => assert!(set.insert_check(&i)),
-                _ => assert!(!set.insert_check(&i)),
-            }
-        }
-        assert_eq!(set.len(), max_elts / 2);
-
-        for i in 0..max_elts {
-            match i % 2 {
-                0 => assert!(set.insert_check(&i)),
-                _ => assert!(!set.insert_check(&i)),
-            }
-        }
-        assert_eq!(set.len(), max_elts);
     }
 }
--- src/bin/search_boards.rs
+++ src/bin/search_boards.rs
@@ -1,9 +1,7 @@
 use rayon::prelude::*;
 
 #[allow(unused_imports)]
-use tile_game::big_set::{
-    BigSet, BloomSet, HashedItemSet, InteriorMutableSet, ParallelSet, PartitionSet,
-};
+use tile_game::big_set::{HashedItemSet, LittleSet};
 
 use tile_game::big_stack::{BigStack, Stack};
 
@@ -18,7 +16,7 @@ fn factorial(x: usize) -> usize {
 
 fn main() {
     // This the width and height of the tile game we're using.
-    let (w, h) = (3, 4);
+    let (w, h) = (3, 3);
     let n_elts = (w * h) as u8;
     let n_solns = factorial(w * h) / 2;
     println!("Board size: {w}x{h}");
@@ -28,54 +26,6 @@ fn main() {
     let n_solns = find_all_boards_iteratively(board, n_solns);
     // let n_solns = find_all_boards_in_parallel(board);
     println!("There are {} solutions.", n_solns);
-}
-
-/// Counts the number of boards which are accessible from the starting position.
-fn find_all_boards_in_parallel(board: Board) -> usize {
-    let stack_cache_size = 1 << 24;
-    let mut unprocessed_boards: BigStack<Board> = BigStack::new(stack_cache_size);
-    unprocessed_boards.push(board.clone());
-
-    let set_cache = 1 << 22;
-    let n_partitions = 4;
-    let all_boards: Arc<ParallelSet<Board>> =
-        { Arc::new(ParallelSet::with_prime_caches(set_cache, n_partitions)) };
-
-    while unprocessed_boards.len() != 0 {
-        let next_unprocessed_boards: Arc<Mutex<BigStack<Board>>> =
-            Arc::new(Mutex::new(BigStack::new(stack_cache_size)));
-
-        loop {
-            let parallel_buffer_len = 1 << 10;
-            let parallel_buffer: Vec<Board> = iter::repeat(())
-                .take(parallel_buffer_len)
-                .map_while(|_| unprocessed_boards.pop())
-                .collect();
-            if parallel_buffer.len() == 0 {
-                break;
-            }
-            parallel_buffer.par_iter().for_each(|board| {
-                if !all_boards.insert_check(&board) {
-                    let mut next_unprocessed_boards = next_unprocessed_boards.lock().unwrap();
-                    for permuted_board in board.slide_iter() {
-                        next_unprocessed_boards.push(permuted_board);
-                    }
-                }
-            })
-        }
-        {
-            let mut next_unprocessed_boards = next_unprocessed_boards.lock().unwrap();
-            mem::swap(&mut unprocessed_boards, &mut next_unprocessed_boards);
-            assert_eq!(next_unprocessed_boards.len(), 0);
-        }
-        println!(
-            "Processed {} boards with {} to go.",
-            all_boards.len(),
-            unprocessed_boards.len()
-        );
-    }
-
-    all_boards.len()
 }
 
 /// Counts the number of boards which are accessible from the starting position.
@@ -89,7 +39,7 @@ fn find_all_boards_iteratively(board: Board, n_solns: usize) -> usize {
     let mut unprocessed_boards: BigStack<Board> = BigStack::new(1 << 25);
     unprocessed_boards.push(board.clone());
 
-    let mut all_boards = PartitionSet::<Board>::new(1 << 18, n_solns, 0.75, 128);
+    let mut all_boards = LittleSet::<Board>::new();
     all_boards.insert(&board);
 
     while let Some(board) = unprocessed_boards.pop() {
